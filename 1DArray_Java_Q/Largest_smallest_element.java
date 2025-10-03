b8551704eefa18e4f76df4356b1b1ecb038ec86d
Paila veera venkata siva kumar <veeravenkatasivakumar.paila@gmail.com>
2025-10-03 10:38:52 +0530

    Create Largest_smallest_element.java

---
+++ 1DArray_Java_Q/Largest_smallest_element.java
@@ -0,0 +1,29 @@
+package practise;
+
+import java.util.*;
+
+public class Largest_smallest_element {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter number of elements: ");
+		int num=sc.nextInt();
+		int arr[]=new int[num];
+		System.out.println("Enter the elements: ");
+		for(int i=0;i<arr.length;i++) {
+			arr[i]=sc.nextInt();
+		}
+		int largest=arr[0];
+		int smallest=arr[0];
+		for(int i=0;i<arr.length;i++) {
+			if(largest<arr[i]) {
+				largest=arr[i];
+			}
+			if(smallest>arr[i]) {
+				smallest=arr[i];
+			}
+		}
+		System.out.println("Largest element in the array is: "+largest);
+		System.out.println("Smallest element in the array is: "+smallest);
+	}
+}
