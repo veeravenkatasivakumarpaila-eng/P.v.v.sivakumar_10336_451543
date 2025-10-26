f0536ffd0135deb120f5e3632c91e4ad5d5a74ba
Paila veera venkata siva kumar <veeravenkatasivakumar.paila@gmail.com>
2025-10-26 11:17:49 +0530

    constants.js

---
+++ Html_Css_Tasks/Ticket_booking/Using_react_live_data/component/constants.js
@@ -0,0 +1,3 @@
+export const IMG_BASE_URL = "https://image.tmdb.org/t/p/w500";
+export const MOVIE_API_URL =
+  "https://api.themoviedb.org/3/movie/popular?api_key=c45a857c193f6302f2b5061c3b85e743&language=en-US&page=1";
