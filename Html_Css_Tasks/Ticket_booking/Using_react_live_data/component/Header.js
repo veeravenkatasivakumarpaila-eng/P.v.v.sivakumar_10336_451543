f4fb0ce7eb98df882c584b7a4e80680754da835d
Paila veera venkata siva kumar <veeravenkatasivakumar.paila@gmail.com>
2025-10-26 11:16:26 +0530

    Header.js

---
+++ Html_Css_Tasks/Ticket_booking/Using_react_live_data/component/Header.js
@@ -0,0 +1,12 @@
+const Header= () =>{
+    return(
+        <div className="container1">
+    <h1>Tollywood Movies</h1>
+    <form>
+        <input type="search" placeholder="Search movies..."/>
+        <button type="submit">search</button>
+    </form>
+    </div>
+    );
+};
+export default Header;
