902cc410be7639731d0fa00c5ac87837691af7ab
Paila veera venkata siva kumar <veeravenkatasivakumar.paila@gmail.com>
2025-10-26 10:57:24 +0530

    FooterComponent.js

---
+++ Html_Css_Tasks/Ticket_booking/Components/FooterComponent.js
@@ -0,0 +1,12 @@
+
+const FooterComponent =()=>{
+    return (
+        <>  
+    <footer className="footer-bottom">
+      <p>© 2025 BookMyMovie. All rights reserved.</p>
+    </footer>
+    </>
+  );
+}
+
+export default FooterComponent;
