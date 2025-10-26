2e62518f558ac911e03cee53fed4399d5b10d6fd
Paila veera venkata siva kumar <veeravenkatasivakumar.paila@gmail.com>
2025-10-26 10:53:33 +0530

    FiltersData.js

---
+++ Html_Css_Tasks/Ticket_booking/React_Tasks/Utils/FiltersData.js
@@ -0,0 +1,5 @@
+
+
+export  const languages = ["English", "Hindi", "Telugu", "Tamil", "Korean"];
+export  const genres = ["Action", "Drama", "Comedy", "Thriller"];
+export  const formats = ["2D", "3D", "IMAX"];
