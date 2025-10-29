ed74a6746ac746b8b7ff7aad94eac87799bb71ef
Paila veera venkata siva kumar <veeravenkatasivakumar.paila@gmail.com>
2025-10-29 10:59:32 +0530

    Create Sorting.java

---
+++ 1DArray_Java_Q/Sorting.java
@@ -0,0 +1,45 @@
+package practise;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Sorting {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter no.of values:");
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		System.out.println("Enter the values:");
+		for(int i=0;i<arr.length;i++) {
+			arr[i]=sc.nextInt();
+		}
+		int copy[]=arr.clone();
+		System.out.println("Entered values are:");
+		for(int i:copy) {
+			System.out.println(i);
+		}
+		Arrays.sort(copy);
+		System.out.println("After sorting: ");
+		for(int i:copy) {
+			System.out.println(i);
+		}
+//		for(int i=0;i<arr.length;i++) {
+//			System.out.println(arr[i]);
+//		}
+	}
+}
+
+//output:
+//Enter no.of values:
+//3
+//Enter the values:
+//6 3 2
+//Entered values are:
+//6
+//3
+//2
+//After sorting: 
+//2
+//3
+//6
