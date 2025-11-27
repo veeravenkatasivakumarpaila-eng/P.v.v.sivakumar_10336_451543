d05450476c1f4f0aba70ea905ecd6fb98143ed43
Paila veera venkata siva kumar <veeravenkatasivakumar.paila@gmail.com>
2025-11-27 18:27:28 +0530

    Add Patient class for hospital management system

---
+++ Hospital Management System/Patient.java
@@ -0,0 +1,29 @@
+package Hospital;
+
+public class Patient {
+
+    private int id;
+    private String name;
+    private int age;
+    private String disease;
+
+    public Patient(int id, String name, int age, String disease) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.disease = disease;
+    }
+
+    public void displayPatient() {
+        System.out.println("Patient ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Disease: " + disease);
+        System.out.println("---------------------------");
+    }
+
+    public int getId() {
+        return id;
+    }
+}
+
