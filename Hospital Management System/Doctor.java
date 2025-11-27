6e89cd2b16f97345a3566ce1bb2e1b73405330ff
Paila veera venkata siva kumar <veeravenkatasivakumar.paila@gmail.com>
2025-11-27 18:26:37 +0530

    Add Doctor class for hospital management system

---
+++ Hospital Management System/Doctor.java
@@ -0,0 +1,25 @@
+package Hospital;
+
+public class Doctor {
+
+    private int id;
+    private String name;
+    private String specialization;
+
+    public Doctor(int id, String name, String specialization) {
+        this.id = id;
+        this.name = name;
+        this.specialization = specialization;
+    }
+
+    public void displayDoctor() {
+        System.out.println("Doctor ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Specialization: " + specialization);
+        System.out.println("---------------------------");
+    }
+
+    public int getId() {
+        return id;
+    }
+}
