1010d178ba6bec34022716bde9aa8f863bfc6ab3
Paila veera venkata siva kumar <veeravenkatasivakumar.paila@gmail.com>
2025-12-03 12:48:34 +0530

    Update Sqlcon1.java

--- jdbc/Basic_connectivity_program/Sqlcon1.java
+++ jdbc/Basic_connectivity_program/Sqlcon1.java
@@ -1,20 +1,35 @@
 package jdbc;
 
 import java.sql.*;
-public class Sqlcon1 {
+
+
+public class sqlcon1 {
 	static String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
-	static String DB_URL = "jdbc:mysql://localhost:3306/one";
+	static String DB_URL = "jdbc:mysql://localhost:3306/siva1";
 	static  String DB_USERNAME = "root";
-	static String DB_PASSWORD = "root";
-	public static Connection getcon() throws Exception{
-		Class.forName(DB_DRIVER);
-		Connection con=DriverManager.getConnection(DB_URL,DB_USERNAME,DB_PASSWORD);
-		if(con!=null) {
-			System.out.println("Done");
-		}
-		else {
-			System.out.println("Not done");
-		}
-		return con;
+	static String DB_PASSWORD = "Vizag@123";
+	
+	public static Connection getCon()throws Exception
+	{
+	 Class.forName(DB_DRIVER);
+	 Connection con = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+	 
+	 if(con != null)
+	 {
+		 System.out.println("done");
+	 }else
+	 {
+		 System.out.println("not done");
+	 }
+	 
+	 return con;
 	}
+
+	
+	 public static void main(String[] args) throws Exception {
+		 getCon();
+	 
+		 
+	 }
 }
+
