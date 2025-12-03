fc0c3bdbba555a8f2d9fa5eb6fe5c2e79449b0d7
Paila veera venkata siva kumar <veeravenkatasivakumar.paila@gmail.com>
2025-12-03 12:48:14 +0530

    Update Sqlcon2.java

--- jdbc/Basic_connectivity_program/Sqlcon2.java
+++ jdbc/Basic_connectivity_program/Sqlcon2.java
@@ -1,13 +1,13 @@
 package jdbc;
 
 import java.sql.*;
-public class Sqlcon2 {
+public class sqlcon2 {
 	public static void main(String args[]) throws Exception{
-		Connection con=Sqlcon1.getcon()	;
-		String query="insert into one1 values(?,?,?)";
+		Connection con=sqlcon1.getCon()	;
+		String query="insert into info values(?,?,?)";
 		PreparedStatement ps = con.prepareStatement(query);
 		ps.setInt(1,101);
-		ps.setString(2, "akhi");
+		ps.setString(2, "siva");
 		ps.setString(3, "abc");
 		int i=ps.executeUpdate();
 		if(i>0) {
@@ -16,5 +16,18 @@ public class Sqlcon2 {
 		else {
 			System.out.println("Not done successfully");
 		}
+		PreparedStatement sp = con.prepareStatement("SELECT * FROM info");
+		ResultSet rs = sp.executeQuery();
+		while(rs.next()) {
+			System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3));	
+		}
+		con.close();
 		}
 }
+
+
+
+
+
+
+
